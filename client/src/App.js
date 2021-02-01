bd0ef5a9f9e2bb4c221b3d3f01a9e99ad947d8bc
David Wertheimer <david@wertheimer.io>
2021-01-31 20:39:39 -0500

    changed app.js to see if it actually changes on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,14 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+//          <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Fib Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
